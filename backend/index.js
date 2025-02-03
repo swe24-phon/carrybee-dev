1e64d2e3e4d747107e4d842db96e46d8665d501e
Angela Enriquez <angelaenriquezgar@gmail.com>
2025-02-03 15:02:51 +1100

    backend container working

--- backend/index.js
+++ backend/index.js
@@ -14,13 +14,13 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-})
-
-// test API
-app.get('/test', (req, res) => {
-    try {
-        res.status(200).json({ message: 'Api is working' });
-    } catch {
-        res.status(500).json({ message: error.message });
-    }
+    next();
 });
+
+app.get('/', (req, res) => {
+    res.send('Hello Carry Bee!');
+});
+
+//START the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
