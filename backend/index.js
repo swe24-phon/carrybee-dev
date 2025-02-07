688b46324c894fce7720d018c27da9aa5bafaf39
Angela Enriquez <angelaenriquezgar@gmail.com>
2025-02-07 20:33:58 +1100

    database fixed

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,12 @@ const app = express();
 app.use(express.json());
 
 // avoid any cors problem
-app.use(cors({
-    origin: '*', // Allow all origins (use specific domains in production)
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send('Hello Carry Bee!');
