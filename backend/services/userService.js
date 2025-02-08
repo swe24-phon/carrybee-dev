72722476df3c9e7570d38d54c4a6783f2c1b35d9
Angela Enriquez <angelaenriquezgar@gmail.com>
2025-02-08 15:25:00 +1100

    updating schema with pickup date/time

---
+++ backend/services/userService.js
@@ -0,0 +1,10 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const bcrypt = require('bcrypt');
+
+// Create User
+const createUser = async (userData) => {
+    try { 
+        const {}
+    }
+}
