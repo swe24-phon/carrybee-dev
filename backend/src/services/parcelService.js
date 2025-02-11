3e3ed6f70bbbbbf3dadf36db18b4c561303e4c12
Angela Enriquez <angelaenriquezgar@gmail.com>
2025-02-11 16:17:56 +1100

    using a custom instance of PrismaClient on Services files

--- backend/src/services/parcelService.js
+++ backend/src/services/parcelService.js
@@ -1,5 +1,5 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const prisma = require('../prismaClient');
+
 
 // Create Parcel
 const createParcel = async (parcelData) => {
--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -1,5 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const prisma = require('../prismaClient');
 const bcrypt = require('bcrypt');
 const validator = require('validator')
 
