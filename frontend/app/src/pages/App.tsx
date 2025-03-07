88189577af2bcbabb742063e0a83a085b705176c
chutima puthachon <chutima.p8@gmail.com>
2025-03-07 21:34:01 +1100

    added payment form

--- frontend/app/src/pages/App.tsx
+++ frontend/app/src/pages/App.tsx
@@ -53,6 +53,7 @@ import Dashboard from './Dashboard';
 import PaymentSuccess from './PaymentSuccess';
 import PaymentError from './PaymentError';
 import SignupComponent from '../components/SignupComponent';
+import PaymentForm from './PaymentForm';
 
 const stripePromise = loadStripe('process.env.STRIPE_PUBLISHABLE_KEY');
 
@@ -66,13 +67,14 @@ const App: React.FC = () => {
             <Route path="/signup" element={<SignupComponent />} />
             <Route path="/" element={<SignInComponent />} />
             <Route path="/homepage" element={<HomePage />} />
-            <Route path="/Schedule" element={<Schedule />} />
-            <Route path="/Form" element={<Form />} />
-            <Route path="/Vehicle" element={<Vehicle />} />
-            <Route path="/Payment" element={<PaymentComponent />} />
-            <Route path="/Dashboard" element={<Dashboard />} />
+            <Route path="/schedule" element={<Schedule />} />
+            <Route path="/form" element={<Form />} />
+            <Route path="/vehicle" element={<Vehicle />} />
+            <Route path="/payment" element={<PaymentComponent />} />
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/payment-success" element={<PaymentSuccess />} />
             <Route path="/payment-error" element={<PaymentError />} />
+            <Route path="/payment-form" element={<PaymentForm />} />
           </Routes>
         </Router>
       </Elements>
